11a1ca536c153e1cae7ae4d84030b2d4df135081
Billy Liu <billyliu.cool@163.com>
2021-01-15 17:48:13 +0800

    Add crash track

--- ExceptionGroup/MainActivity.cs
+++ ExceptionGroup/MainActivity.cs
@@ -17,6 +17,8 @@ namespace ExceptionGroup
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
+        int i = 0;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -31,6 +33,85 @@ namespace ExceptionGroup
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button_Click;
+
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            button2.Click += Button2_Click;
+
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            button3.Click += Button3_Click;
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+           
+
+            try
+            {
+                C c = null;
+                Console.WriteLine(c.pet);
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+
+                //var messageWithStacktraceId = exception.Message + $" Stack ID: [{exception.StackTrace.GetHashCode()}]";
+                //var wrappedException = new WrappedException(messageWithStacktraceId, exception.StackTrace);
+
+                //Crashes.TrackError(wrappedException, null);
+
+
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                B b = null;
+                Console.WriteLine(b.age);
+            }
+            catch (Exception exception)
+            {
+                if (i % 2 == 1)
+                {
+                    TimeZoneNotFoundException e3 = new TimeZoneNotFoundException(exception.Message + "Error3");
+                    Crashes.TrackError(e3,null, new ErrorAttachmentLog[]{ErrorAttachmentLog.AttachmentWithText("1!", "hello.txt")}); 
+                }
+                else
+                {
+                    TimeZoneNotFoundException e2 = new TimeZoneNotFoundException(exception.Message + "Error4");
+                    Crashes.TrackError(e2, null, new ErrorAttachmentLog[] { ErrorAttachmentLog.AttachmentWithText("2!", "hello.txt") });
+                }
+                i++;
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                A a = null;
+                Console.WriteLine(a.name);
+
+
+            }
+            catch (Exception exception)
+            {
+                TimeZoneNotFoundException e2 = new TimeZoneNotFoundException(exception.Message+"Button1");
+                Crashes.TrackError(e2);
+
+                //var messageWithStacktraceId = exception.Message + $" Stack ID: [{exception.StackTrace.GetHashCode()}]";
+                //var wrappedException = new WrappedException(messageWithStacktraceId, exception.StackTrace);
+
+                //Crashes.TrackError(wrappedException, null);
+
+
+            }
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -54,15 +135,18 @@ namespace ExceptionGroup
         {
             try
             {
-                // your code here.
+                int divByZero = 42 / int.Parse("0");
             }
             catch (Exception exception)
             {
-                var properties = new Dictionary<string, string> {
-                    { "Category", "Music" },
-                    { "Wifi", "On" }
-                };
-                Crashes.TrackError(exception, properties);
+                //Crashes.TrackError(exception);
+          
+                //var messageWithStacktraceId = exception.Message + $" Stack ID: [{exception.StackTrace.GetHashCode()}]";
+                //var wrappedException = new WrappedException(messageWithStacktraceId, exception.StackTrace);
+
+                //Crashes.TrackError(wrappedException, null);
+
+
             }
 
             View view = (View)sender;
@@ -76,5 +160,17 @@ namespace ExceptionGroup
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+    }
+    public class A
+    {
+        public string name { get; set; }
+    }
+    public class B
+    {
+        public string age { get; set; }
+    }
+    public class C
+    {
+        public string pet { get; set; }
     }
 }
